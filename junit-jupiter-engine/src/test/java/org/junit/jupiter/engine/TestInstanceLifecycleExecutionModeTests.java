b414b9a3b4f392dcd147635719d0351a2f45cf14
Marc Philipp <mail@marcphilipp.de>
2018-08-10 22:01:41 +0200

    Lifecycle.PER_CLASS ⇒ ExecutionMode.SAME_THREAD
    Tests in classes that use `Lifecycle.PER_CLASS` now use
    `ExecutionMode.SAME_THREAD` by default when parallel execution is
    enabled. In the case of nested classes, as soon as one enclosing class
    uses `Lifecycle.PER_CLASS` all contained nested classes and tests use
    `ExecutionMode.SAME_THREAD`.
    
    Fixes #1511.

---
+++ junit-jupiter-engine/src/test/java/org/junit/jupiter/engine/TestInstanceLifecycleExecutionModeTests.java
@@ -0,0 +1,118 @@
+/*
+ * Copyright 2015-2018 the original author or authors.
+ *
+ * All rights reserved. This program and the accompanying materials are
+ * made available under the terms of the Eclipse Public License v2.0 which
+ * accompanies this distribution and is available at
+ *
+ * http://www.eclipse.org/legal/epl-v20.html
+ */
+
+package org.junit.jupiter.engine;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.TestInstance.Lifecycle.PER_CLASS;
+import static org.junit.platform.commons.util.CollectionUtils.getOnlyElement;
+import static org.junit.platform.engine.discovery.DiscoverySelectors.selectClass;
+import static org.junit.platform.engine.support.hierarchical.Node.ExecutionMode.CONCURRENT;
+import static org.junit.platform.engine.support.hierarchical.Node.ExecutionMode.SAME_THREAD;
+
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
+import org.junit.jupiter.engine.descriptor.ClassTestDescriptor;
+import org.junit.jupiter.engine.descriptor.NestedClassTestDescriptor;
+import org.junit.jupiter.engine.descriptor.TestMethodTestDescriptor;
+import org.junit.platform.engine.TestDescriptor;
+import org.junit.platform.engine.support.hierarchical.Node;
+
+class TestInstanceLifecycleExecutionModeTests extends AbstractJupiterTestEngineTests {
+
+	@Test
+	void methodsInTestClassesWithInstancePerClassHaveExecutionModeSameThread() {
+		var engineDescriptor = discoverTests(selectClass(SimpleTestCase.class));
+		var classDescriptor = (Node<?> & TestDescriptor) getOnlyElement(engineDescriptor.getChildren());
+		var testDescriptor = (Node<?> & TestDescriptor) getOnlyElement(classDescriptor.getChildren());
+
+		assertExecutionMode(classDescriptor, CONCURRENT);
+		assertExecutionMode(testDescriptor, SAME_THREAD);
+	}
+
+	@Test
+	void methodsInNestedTestClassesWithInstancePerClassInHierarchyHaveExecutionModeSameThread() {
+		var engineDescriptor = discoverTests(selectClass(OuterTestCase.class));
+		var outerTestCaseClassDescriptor = firstChild(engineDescriptor, ClassTestDescriptor.class);
+		var outerTestMethodDescriptor = firstChild(outerTestCaseClassDescriptor, TestMethodTestDescriptor.class);
+		var levelOneNestedClassDescriptor = firstChild(outerTestCaseClassDescriptor, NestedClassTestDescriptor.class);
+		var levelOneTestMethodDescriptor = firstChild(levelOneNestedClassDescriptor, TestMethodTestDescriptor.class);
+		var levelTwoNestedClassDescriptor = firstChild(levelOneNestedClassDescriptor, NestedClassTestDescriptor.class);
+		var levelTwoTestMethodDescriptor = firstChild(levelTwoNestedClassDescriptor, TestMethodTestDescriptor.class);
+		var levelThreeNestedClassDescriptor = firstChild(levelTwoNestedClassDescriptor,
+			NestedClassTestDescriptor.class);
+		var levelThreeTestMethodDescriptor = firstChild(levelThreeNestedClassDescriptor,
+			TestMethodTestDescriptor.class);
+
+		assertExecutionMode(outerTestCaseClassDescriptor, CONCURRENT);
+		assertExecutionMode(outerTestMethodDescriptor, CONCURRENT);
+		assertExecutionMode(levelOneNestedClassDescriptor, CONCURRENT);
+		assertExecutionMode(levelOneTestMethodDescriptor, CONCURRENT);
+		assertExecutionMode(levelTwoNestedClassDescriptor, CONCURRENT);
+		assertExecutionMode(levelTwoTestMethodDescriptor, SAME_THREAD);
+		assertExecutionMode(levelThreeNestedClassDescriptor, SAME_THREAD);
+		assertExecutionMode(levelThreeTestMethodDescriptor, SAME_THREAD);
+	}
+
+	private static <T extends Node<?> & TestDescriptor> void assertExecutionMode(T testDescriptor,
+			Node.ExecutionMode concurrent) {
+		assertThat(testDescriptor.getExecutionMode()).describedAs("ExecutionMode for %s", testDescriptor).isEqualTo(
+			concurrent);
+	}
+
+	@SuppressWarnings("unchecked")
+	private <T extends Node<?> & TestDescriptor> T firstChild(TestDescriptor engineDescriptor,
+			Class<T> testDescriptorClass) {
+		return (T) engineDescriptor.getChildren().stream() //
+				.filter(testDescriptorClass::isInstance) //
+				.findFirst() //
+				.orElseGet(() -> fail("No child with class " + testDescriptorClass + " found"));
+	}
+
+	@TestInstance(PER_CLASS)
+	static class SimpleTestCase {
+
+		@Test
+		void test() {
+		}
+
+	}
+
+	static class OuterTestCase {
+		@Nested
+		class LevelOne {
+			@Nested
+			@TestInstance(PER_CLASS)
+			class LevelTwo {
+				@Nested
+				class LevelThree {
+					@Test
+					void test() {
+					}
+				}
+
+				@Test
+				void test() {
+				}
+			}
+
+			@Test
+			void test() {
+			}
+		}
+
+		@Test
+		void test() {
+		}
+	}
+
+}
